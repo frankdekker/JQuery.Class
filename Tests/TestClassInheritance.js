ac54667974b57da77dbce32ffca3f7de8bdff5f3
F. Dekker <fdekker@gmail.com>
2016-03-05 00:14:34 +0100

    Unittest better coverage

--- Tests/TestClassInheritance.js
+++ Tests/TestClassInheritance.js
@@ -2,6 +2,8 @@ var TestClassInheritanceResult = [];
 
 var TestClassInheritance1 = new Class({
 
+	var1: null,
+
 	test1: function()
 	{
 		TestClassInheritanceResult.push(4);
@@ -15,12 +17,19 @@ var TestClassInheritance1 = new Class({
 	test3: function()
 	{
 		return 'unittest';
+	},
+
+	test4: function()
+	{
+		this.var1 = 'varunittest1';
 	}
 
 });
 
 var TestClassInheritance2 = new Class({
 	Extends: TestClassInheritance1,
+
+	var2: null,
 
 	test1: function()
 	{
@@ -35,13 +44,22 @@ var TestClassInheritance2 = new Class({
 		this.parentMethod();
 	},
 	
-	test3: function() {
+	test3: function()
+	{
 		return this.parentMethod();
+	},
+
+	test4: function()
+	{
+		this.parentMethod();
+		this.var2 = 'varunittest2';
 	}
 });
 
 var TestClassInheritance3 = new Class({
 	Extends: TestClassInheritance2,
+
+	var3: null,
 
 	test1: function()
 	{
@@ -54,6 +72,12 @@ var TestClassInheritance3 = new Class({
 	test3: function() 
 	{
 		return this.parentMethod();	
+	},
+
+	test4: function()
+	{
+		this.parentMethod();
+		this.var3 = 'varunittest3';
 	}
 
 });
@@ -64,14 +88,21 @@ QUnit.test("Inheritance", function(assert)
 	// single inheritance
 	var testClassInheritance = new TestClassInheritance2();
 	testClassInheritance.test1();
+	testClassInheritance.test4();
 	assert.deepEqual([1, 2, 3, 4], TestClassInheritanceResult);
-	assert.equals('unittest', testClassInheritance.test3());
+	assert.equal('unittest', testClassInheritance.test3());
+	assert.equal('varunittest1', testClassInheritance.var1);
+	assert.equal('varunittest2', testClassInheritance.var2);
 
 	// double inheritance
 	TestClassInheritanceResult = [];
 	testClassInheritance = new TestClassInheritance3();
 	testClassInheritance.test1();
+	testClassInheritance.test4();
 	
 	assert.deepEqual([0, 1, 2, 3, 4, 2, 3, 1, 2, 3, 4], TestClassInheritanceResult);
-	assert.equals('unittest', testClassInheritance.test3());
+	assert.equal('unittest', testClassInheritance.test3());
+	assert.equal('varunittest1', testClassInheritance.var1);
+	assert.equal('varunittest2', testClassInheritance.var2);
+	assert.equal('varunittest3', testClassInheritance.var3);
 });
